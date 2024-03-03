19e05d759e5a18591bb0de446a16accc78bf1721
wijaemin <minialp2002@naver.com>
2024-03-03 17:17:18 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,24 +1,40 @@
 "use strict";
 
 
-const output = {
 
+
+const output = {
     home : (req,res)=>{
         res.render("home/index");
     },
-    
     login : (req,res)=>{
         res.render("home/login");
     },
 };
 
+const users ={
+    id : [ 'test1', 'test2', 'test3'],
+    password : [ 'test1!', 'test2!', 'test3!'],
+}
 const process = {
-
     login:(req,res) =>{
-        console.log(req.body);
+       const id = req.body.id,
+            password = req.body.password;
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password) {
+                return res.json({
+                    success:true,
+                    
+                })
+            }
+        }
+        return res.json({
+            success:false,
+            msg: "로그인 실패",
+        });
     },
 };
-
 
 module.exports = {
     output,
