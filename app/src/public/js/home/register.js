9b53ca5402c513c225e08a71b7e18780dca4c287
wijaemin <minialp2002@naver.com>
2024-03-03 20:38:50 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,17 +1,22 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     password = document.querySelector("#password"),
-    loginBtn = document.querySelector("#button");
+    confirmPassword = document.querySelector("#confirm-password"),
+    registerBtn = document.querySelector("#button");
+    
+registerBtn.addEventListener("click", register);
 
-loginBtn.addEventListener("click", login);
-
-function login(){
+function register(){
     const req ={
         id : id.value,
+        name : name.value,
         password : password.value,
+        confirmPassword : confirmPassword.value,
     };
-    fetch("/login",{
+    console.log(req);
+    fetch("/register",{
         method : "POST",
         headers : {
             "Content-Type" :"application/json",
@@ -20,13 +25,13 @@ function login(){
     }).then((res)=>res.json())
     .then((res) =>{
         if(res.success) {
-            location.href ="/";
+            location.href ="/login";
         }
         else{
             alert(res.msg);
         }
     })
     .catch((err)=>{
-        console.error(new Error("로그인 중 에러 발생"));
+        console.error(new Error("회원가입 중 에러 발생"));
     });
 }
