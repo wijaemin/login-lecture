fb729dd3796d09bb73e20159bfcdaf950a6b9dbb
wijaemin <minialp2002@naver.com>
2024-03-03 19:08:38 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -18,6 +18,17 @@ class UserStorage {
         
         return  newUsers;
     }
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx =users.id.indexOf(id);//getUserInfo(id)에서 받아온 id 값으로 users의 id랑 같은 index값을 가져옴
+        const usersKeys = Object.keys(users); //users의 키값들만 usersKeys에 배열 형태로 넣음=>[id,password,name]
+        const userInfo =usersKeys.reduce((newUser,info) =>{ //newUser라는 오브젝트에 info = usersKeys의 키 값 첫번째부터 끝까지 넣음
+            newUser[info] =users[info][idx];//users의 info(=키 값)의 index에 해당하는 값을 전부 newUser[info]에 넣음
+            return newUser;
+        },{});
+
+        return userInfo;
+    };
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
 
 const output = {
     home : (req,res)=>{
@@ -14,22 +14,11 @@ const output = {
 
 const process = {
     login:(req,res) =>{
-        const id = req.body.id,
-            password = req.body.password;
+       const user = new User(req.body);
 
-        const users =UserStorage.getUsers('id','password');
-        const response = {};
+       const response = user.login();
 
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password) {
-                response.success = true;
-                return res.json(response)
-            }
-        }
-        response.success = false;
-        response.msg = "로그인 실패";
-        return res.json(response);
+       return res.json(response);
     },
 };
 
