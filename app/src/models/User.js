83ecd8f82ad2cfd9cbb54b078348dd1eaceb689c
wijaemin <minialp2002@naver.com>
2024-03-04 19:25:51 +0900

    fs 모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -22,10 +22,18 @@ class User {
         return {success : false, msg: "존재하지 않는 아이디"};
 
     };
-    register(){
+    async register(){
         const client = this.body;
-        const response = UserStorage.save(client);
-        return response;
+        try{
+            const response = await UserStorage.save(client);
+            console.log("response=" + response);
+            return response;
+        }
+        catch(err){
+            
+            return { success: false, msg: err };
+
+        }
     }
 }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -14,9 +14,9 @@ class UserStorage {
         return userInfo;
     }
 
-    
-    static getUsers(...fields) {
-        // const users = this.#users;
+    static #getUsers(data, isAll, fields) {
+        const users=JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers,field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -25,6 +25,16 @@ class UserStorage {
         },{});
         
         return  newUsers;
+
+    }
+    
+    static getUsers(isAll, ...fields) {
+
+        return fs.readFile("./src/databases/users.json")
+        .then((data)=>{
+            return this.#getUsers(data, isAll, fields);
+        })
+        .catch(console.error); 
     }
 
     static getUserInfo(id){
@@ -38,11 +48,18 @@ class UserStorage {
 
 
 
-    static save(userInfo){
-        // const users = this.#users;
+    static async save(userInfo){
+        // const users =await this.getUsers("id", "password", "name");모든 데이터 가져오고 싶을 때 true
+        const users =await this.getUsers(true);
+        // console.log(users);모든 데이터 가져오고 싶을 때 true
+        if(users.id.includes(userInfo.id)){
+            throw "이미 존재하는 아이디입니다";
+        }
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
+        //데이터 추가
+        fs.writeFile("./src/databases/users.json",JSON.stringify(users));
         return {success: true};
     }
 }
